0d26e4a0fd490e40a1aab2c94df8f1e1ea65dfcc
Thyssenkrupp234 <Thyssenkrupp234@gmail.com>
2022-05-03 12:51:44 -0700

    I think im almost done

--- js/garandbake.js
+++ js/garandbake.js
@@ -1,6 +1,6 @@
 const teleportbutton = document.getElementById('buttonpageteleport');
 
 teleportbutton.onclick = function() {
-    alert('You are being teleported to the button page..');
-    location.href = "https://www.oslerleadership.com/fundraisers/formbuttonpage.html";
+        location.href = "https://www.oslerleadership.com/fundraisers/formbuttonpage.html";
+        alert('You are being teleported to the button page..');
 }
