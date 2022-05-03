9a0ccad4bcacda095ed41695b17c93f6e2d663c2
Thyssenkrupp234 <Thyssenkrupp234@gmail.com>
2022-05-03 12:55:37 -0700

    Why isn't this updating lol

--- js/garandbake.js
+++ js/garandbake.js
@@ -2,5 +2,5 @@ const teleportbutton = document.getElementById('buttonpageteleport');
 
 teleportbutton.onclick = function() {
         location.href = "https://www.oslerleadership.com/fundraisers/formbuttonpage.html";
-        alert('Click ok to confirm teleporting to the OSLER Fundraiser Form');
+        alert('Click ok to confirm teleporting to the OSLER Fundraiser Form.');
 }
