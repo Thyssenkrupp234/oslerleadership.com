17a1ffa8f1c611920cc11d16e2f85cd795c36aa2
Thyssenkrupp234 <Thyssenkrupp234@gmail.com>
2022-05-03 12:56:20 -0700

    fix

--- js/garandbake.js
+++ js/garandbake.js
@@ -1,6 +1,6 @@
 const teleportbutton = document.getElementById('buttonpageteleport');
 
-teleportbutton.onclick = function() {
-        location.href = "https://www.oslerleadership.com/fundraisers/formbuttonpage.html";
-        alert('Click ok to confirm teleporting to the OSLER Fundraiser Form.');
+teleportbutton.onclick = function() {        
+    alert('Click ok to confirm teleporting to the OSLER Fundraiser Form.');
+    location.href = "https://www.oslerleadership.com/fundraisers/formbuttonpage.html";
 }
