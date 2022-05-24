92d65faa975801599f141957cffc29ea413bc4aa
Lincoln <thyssenkrupp234@gmail.com>
2022-05-23 21:25:19 -0700

    Bake sale info

--- js/garandbake.js
+++
@@ -1,6 +0,0 @@
-const teleportbutton = document.getElementById('buttonpageteleport');
-
-teleportbutton.onclick = function() {        
-    alert('Click ok to confirm teleporting to the OSLER Fundraiser Form.');
-    location.href = "https://www.oslerleadership.com/fundraisers/formbuttonpage.html";
-}
